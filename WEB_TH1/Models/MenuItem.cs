9c0704fa96586d1ff8cc756a397337f047d316d8
Minh Uyen Nguyen <nguyenthithuuyen03022003@gmail.com>
2023-09-20 16:44:44 +0700

    thuc hanh buoi 2

---
+++ WEB_TH1/Models/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace WEB_TH1.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }//Item id
+        public string Name { get; set; } //Item name
+        public string Link { get; set; } //Item label
+    }
+}
---
+++ WEB_TH1/ViewComponents/RenderViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEB_TH1.Models;
+
+namespace WEB_TH1.ViewComponents 
+{
+    
+    public class RenderViewComponent : ViewComponent
+    {
+        private List<MenuItem> MenuItems = new List<MenuItem>();
+
+        public RenderViewComponent() 
+        {
+            MenuItems = new List<MenuItem>()
+            {
+                new MenuItem() {Id =1, Name="Branches", Link="Branches/List"},
+                new MenuItem() {Id =2, Name="Student", Link="Student/List"},
+                new MenuItem() {Id =3, Name="Subjects", Link="Subjects/List"},
+                new MenuItem() {Id =4, Name="Cources", Link="Cources/List"}
+            };
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("RenderLeftMenu", MenuItems);
+        }
+    }
+}
